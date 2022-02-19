765cfc381aca7f551ed08df7161cf7eae1372d28
RedaMeziouni <meziounidb@gmail.com>
2022-02-19 19:14:25 +0100

    JS starter

---
+++ app.js
@@ -0,0 +1,6 @@
+// Acceding to the canvas
+const canvas = document.getElementById('canvas');
+// Leading us to the creation of a CanvasRenderingContext 2D object representing a two-dimensional rendering context
+const ctx = canvas.getContext('2d');
+
+
